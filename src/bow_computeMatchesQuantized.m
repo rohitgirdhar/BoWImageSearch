50144f38026406bcbefa13b0819bc287b7aafd89
Rohit Girdhar <rohit.iiith@gmail.com>
2014-08-28 13:36:52 -0400

    fixed matching and visualize. matching m:n

--- src/bow_computeMatchesQuantized.m
+++ src/bow_computeMatchesQuantized.m
@@ -4,19 +4,24 @@ function matches = bow_computeMatchesQuantized(d1, d2)
 % (same output format as vl_ubcmatch)
 
 %% Hash one of descriptors
-%%% TODO : Same key can be at multiple locations. Handle that.
-keyset = d1;
-valueset = 1 : size(d1, 2);
-c = containers.Map(keyset, valueset);
-
-%% Set matches
-matches = zeros(2, size(d1, 2));
-matches_count = 1;
-for i = 1 : size(d2, 2)
-    if c.isKey(d2(1, i))
-        matches(1, matches_count) = c(d2(1, i));
-        matches(2, matches_count) = i;
-        matches_count = matches_count + 1;
+c = containers.Map('KeyType', 'int64', 'ValueType', 'any');
+for i = 1 : numel(d1)
+    if c.isKey(d1(i))
+        c(d1(i)) = [c(d1(i)), i];
+    else
+        c(d1(i)) = i;
     end
 end
-matches = matches(:, 1:matches_count - 1);
+
+%% Set matches
+matches_count = 1;
+for i = 1 : size(d2, 2)
+    if c.isKey(d2(i))
+        matches_list = c(d2(i));
+        for j = 1 : numel(matches_list)
+            matches(1, matches_count) = matches_list(j);
+            matches(2, matches_count) = i;
+            matches_count = matches_count + 1;
+        end
+    end
+end
--- src/bow_visualizeMatching.m
+++ src/bow_visualizeMatching.m
@@ -1,15 +1,19 @@
 function bow_visualizeMatching(I1, I2, f1, f2, matches)
+% I1, I2 are the images
+% f1, f2 are feature keypoints, as detected by vl_sift
+% matches are between f1 and f2, as detected by vl_ubcmatch or
+% bow_computeMatchesQuantized
 
 bow_config;
 
 I = appendimages(I1, I2);
-figure('Position', [300 300 size(I, 2) size(I, 1)]);
+figure('Position', [100 100 size(I, 2) size(I, 1)]);
 imagesc(I);
 hold on;
 cols1 = size(I1, 2);
 for i = 1 : size(matches, 2)
-    X = [f1(2, matches(1, i)); f2(2, matches(2, i)) + cols1];
-    Y = [f1(1, matches(1, i)); f2(1, matches(2, i))];
+    X = [f1(1, matches(1, i)); f2(1, matches(2, i)) + cols1];
+    Y = [f1(2, matches(1, i)); f2(2, matches(2, i))];
     line(X, Y, 'Color', 'c');
 end
 hold off;
